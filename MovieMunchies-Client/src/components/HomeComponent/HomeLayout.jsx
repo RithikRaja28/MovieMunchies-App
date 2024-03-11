55348d7ae4c306d6d5e652116c4f0af52f85102c
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-11 22:56:52 +0530

    Update HomeLayout.jsx

--- MovieMunchies-Client/src/components/HomeComponent/HomeLayout.jsx
+++ MovieMunchies-Client/src/components/HomeComponent/HomeLayout.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 import NavbarLayout from "../Navbar Component/NavbarLayout";
 import "./Home.css";
 const HomeLayout = () => {
+  const [itemCounts, setItemCounts] = useState({
+    beverages: 0,
+    popcornCoke: 0,
+    chilliSauceSamosa: 0,
+    burgerFries: 0,
+    kidsPackCombo: 0,
+    chatMixture: 0,
+    veganMomos: 0,
+    paaniPuriChat: 0,
+    choclateIcecream: 0,
+    chociBrownieVanillaIceCream: 0,
+    chocoVanillaIceCream: 0,
+    oreoStrawberryCupCakes: 0,
+  });
+  const addItemEvent = (foodItem) => {
+    setItemCounts((prevCounts) => ({
+      ...prevCounts,
+      [foodItem]: prevCounts[foodItem] + 1,
+    }));
+  };
   return (
     <div>
       <div className="container-fluid m-2">
@@ -30,7 +51,7 @@ const HomeLayout = () => {
             </nav>
           </div>
           <div className="col text-end p-4 ms-3 mt-5">
-            <button className="cart-btn mt-3">
+            <button className="cart-btn mt-3 ">
               <img
                 src="trolley.png"
                 alt="cart"
@@ -54,11 +75,16 @@ const HomeLayout = () => {
                   alt=""
                 />
                 <h5 class="card-title m-2">Beverages</h5>
-                <p class="card-text">3 Combos*</p>
+                <p class="card-text">Special Combo Available * [3 Combo]</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">299/-</span>
+                  <button
+                    onClick={() => addItemEvent("beverages")}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.beverages} items</span>
                 </div>
               </div>
             </div>
@@ -74,9 +100,14 @@ const HomeLayout = () => {
                 <h5 class="card-title m-2">Popcorn + Coke</h5>
                 <p class="card-text">Special offer Available *</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">480/-</span>
+                  <button
+                    class="btn btn-danger"
+                    onClick={() => addItemEvent("popcornCoke")}
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.popcornCoke} items</span>
                 </div>
               </div>
             </div>
@@ -92,9 +123,19 @@ const HomeLayout = () => {
                 <h5 class="card-title">Chilli Sauce - Samosa</h5>
                 <p class="card-text mt-3">9.pcs of Samosa + Chilli Sause</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">112/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("chilliSauceSamosa");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2">
+                    {" "}
+                    {itemCounts.chilliSauceSamosa} items
+                  </span>
                 </div>
               </div>
             </div>
@@ -108,11 +149,18 @@ const HomeLayout = () => {
                   alt=""
                 />
                 <h5 class="card-title m-2">Chicken Burger + French Fries</h5>
-                <p class="card-text mt-2">[Chicken , Mutton] Burger *</p>
+                <p class="card-text mt-2">[Chicken , Mutton]</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">300/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("burgerFries");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.burgerFries} items</span>
                 </div>
               </div>
             </div>
@@ -128,9 +176,16 @@ const HomeLayout = () => {
                 <h5 class="card-title m-2">Kids Pack Combo</h5>
                 <p class="card-text mt-3">Ice Cola + Candies + Sweet Popcorn</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">499/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("kidsPackCombo");
+                    }}
+                    className="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.kidsPackCombo} items</span>
                 </div>
               </div>
             </div>
@@ -148,9 +203,18 @@ const HomeLayout = () => {
                   Indian Style Mixture [Spicy , Sour]
                 </p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">80/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("chatMixture");
+                    }}
+                    className="btn btn-danger"
+                    class="btn btn-danger"
+                  >
+                    {" "}
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.chatMixture} items</span>
                 </div>
               </div>
             </div>
@@ -164,11 +228,20 @@ const HomeLayout = () => {
                   alt=""
                 />
                 <h5 class="card-title m-2">Vegan Momos</h5>
-                <p class="card-text mt-3">Veggie Vegan momos</p>
+                <p class="card-text mt-3">
+                  Veggie Vegan momos [Fine Vegetables]
+                </p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">119/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("veganMomos");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.veganMomos} items</span>
                 </div>
               </div>
             </div>
@@ -184,9 +257,16 @@ const HomeLayout = () => {
                 <h5 class="card-title m-2">Paani Puri - Chat</h5>
                 <p class="card-text mt-3">[Spicy,Sweet] Paani + Cruncy puri</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">50/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("paaniPuriChat");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2"> {itemCounts.paaniPuriChat} items</span>
                 </div>
               </div>
             </div>
@@ -200,11 +280,23 @@ const HomeLayout = () => {
                   alt=""
                 />
                 <h5 class="card-title m-2">Choclate Icecream</h5>
-                <p class="card-text mt-3">Special Combo Available *</p>
+                <p class="card-text mt-3">
+                  Special Combo Available * [+ Brownie , Fruit, IceCream]
+                </p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">150/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("choclateIcecream");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2">
+                    {" "}
+                    {itemCounts.choclateIcecream} items
+                  </span>
                 </div>
               </div>
             </div>
@@ -220,9 +312,20 @@ const HomeLayout = () => {
                 <h5 class="card-title m-2">Choci Brownie + Vanilla IceCream</h5>
                 <p class="card-text mt-3">Special Combo Available*</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">200/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("chociBrownieVanillaIceCream");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+
+                  <span className="m-2">
+                    {" "}
+                    {itemCounts.chociBrownieVanillaIceCream} items
+                  </span>
                 </div>
               </div>
             </div>
@@ -240,9 +343,19 @@ const HomeLayout = () => {
                 </h5>
                 <p class="card-text mt-3">Special Combo Available*</p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">199/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("chocoVanillaIceCream");
+                    }}
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2">
+                    {" "}
+                    {itemCounts.chocoVanillaIceCream} items
+                  </span>
                 </div>
               </div>
             </div>
@@ -256,15 +369,45 @@ const HomeLayout = () => {
                   alt=""
                 />
                 <h5 class="card-title m-2">Oreo + Strawberry CupCakes</h5>
-                <p class="card-text mt-3">Special Combo Available*</p>
+                <p class="card-text mt-3">
+                  Special Combo Available* [Strawberry,Choci]
+                </p>
                 <div className="text-end">
-                  <button href="#" class="btn btn-danger">
-                    Add
+                  <span class="card-text h5 m-2">290/-</span>
+                  <button
+                    onClick={() => {
+                      addItemEvent("oreoStrawberryCupCakes");
+                    }}
+                    href="#"
+                    class="btn btn-danger"
+                  >
+                    Add ＋
                   </button>
+                  <span className="m-2">
+                    {" "}
+                    {itemCounts.oreoStrawberryCupCakes} items
+                  </span>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="container-fluid mt-5">
+        <div className="text-center">
+          <p className="text-light mt-3">
+            Want to Order from other Restaurants ?
+          </p>
+          <button
+            className="btn btn-warning rounded-pill"
+            onClick={() => {
+              alert(
+                "This Function is under Construction \nStay Healthy 🍿 \n\n\t\t -MovieMunchies"
+              );
+            }}
+          >
+            More Food 🥤
+          </button>
         </div>
       </div>
     </div>
