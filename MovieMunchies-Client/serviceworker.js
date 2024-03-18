60588aacd16174f023425b08f6bd396d74d20f39
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-18 23:51:15 +0530

    PWA

---
+++ MovieMunchies-Client/serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened Cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for request
+self.addEventListener("fetch", (event) => {
+  event.responseWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheNames) => {
+          if (!cacheWhiteList.includes(cacheNames)) {
+            return caches.delete(cacheNames);
+          }
+        })
+      )
+    )
+  );
+});
