787c5a7a930674db7835b41ddc3db6691cfe0a06
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-19 22:49:47 +0530

    Cart Layout

--- MovieMunchies-Client/src/components/Cart Component/CartLayout.jsx
+++ MovieMunchies-Client/src/components/Cart Component/CartLayout.jsx
@@ -1,47 +1,11 @@
-// CartLayout.jsx
-import React, { useState } from "react";
-import Cart from "./Cart";
-import NavbarLayout from "../Navbar Component/NavbarLayout";
-import CartItem from "./Cart";
+import React from 'react'
 
 const CartLayout = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (item) => {
-    setCartItems((prevItems) => [...prevItems, item]);
-  };
-
-  const removeFromCart = (itemId) => {
-    setCartItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
-  };
-
   return (
-    <div className="container-fluid p-3 text-light">
-      <div className="row">
-        <div className="cart-items">
-          <h1>awfawdw</h1>
-          {cartItems.map((item) => (
-            <CartItem
-              key={item.id}
-              item={item}
-              removeFromCart={removeFromCart}
-            />
-          ))}
-        </div>
-        <div className="col-md-4">
-          <NavbarLayout />
-        </div>
-      </div>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          alert("This is the order summary.");
-        }}
-      >
-        Order Now
-      </button>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default CartLayout;
+export default CartLayout
