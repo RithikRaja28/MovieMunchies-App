1b0c1e9ca5e62b76940184972509e9ba198dbffd
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-20 22:38:38 +0530

    Subscription Changes

--- MovieMunchies-Client/src/components/Subscription Component/Subscription.jsx
+++ MovieMunchies-Client/src/components/Subscription Component/Subscription.jsx
@@ -1,11 +1,300 @@
 import React from "react";
 import NavbarLayout from "../Navbar Component/NavbarLayout";
-
+import "./subscription.css";
 const Subscription = () => {
   return (
     <div>
       <div>
         <NavbarLayout />
+      </div>
+      <div className="container-fluid d-flex justify-content-end align-items-end mt-5">
+        <button class="button btn btn-danger mt-5 text-center shake">
+          Get pro now 💵
+        </button>
+      </div>
+      <div className="text-center">
+        <div className="container-sm-2 mt-5 d-flex justify-content-center">
+          <div class="pack-container m-2">
+            <div class="header">
+              <p class="title">Starter</p>
+              <div class="price-container">
+                <span>$</span>29
+                <span>/mo</span>
+              </div>
+            </div>
+            <div>
+              <ul class="lists">
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>500 watch seats</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>500 components</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>2 month free updates</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>2yrs technical support</p>
+                </li>
+              </ul>
+            </div>
+            <div class="button-container">
+              <button type="button btn btn-danger">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="container-sm-2 mt-5 d-flex justify-content-center">
+          <div class="pack-container m-2">
+            <div class="header">
+              <p class="title">Pro</p>
+              <div class="price-container">
+                <span>$</span>499
+                <span>/mo</span>
+              </div>
+            </div>
+            <div>
+              <ul class="lists">
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>1500 Watch Seats</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>1500 components</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>6 month free updates</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>4.5yrs technical support</p>
+                </li>
+              </ul>
+            </div>
+            <div class="button-container">
+              <button type="button btn btn-danger">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="container-sm-2 mt-5 d-flex justify-content-center">
+          <div class="pack-container m-2">
+            <div class="header">
+              <p class="title">Unlimitted</p>
+              <div class="price-container">
+                <span>$</span>9999
+                <span>/mo</span>
+              </div>
+            </div>
+            <div>
+              <ul class="lists">
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>1699 Watch Seats</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>1700 components</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>12 month free updates</p>
+                </li>
+                <li class="list">
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 12.75l6 6 9-13.5"
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <p>Life time technical support</p>
+                </li>
+              </ul>
+            </div>
+            <div class="button-container">
+              <button type="button btn btn-danger">Buy Now</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
