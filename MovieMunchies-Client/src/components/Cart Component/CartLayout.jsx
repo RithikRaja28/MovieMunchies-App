fc1aa0d24fda96dc9becc8a85077245e552e7189
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-25 21:57:03 +0530

    Changes in CartLayout

--- MovieMunchies-Client/src/components/Cart Component/CartLayout.jsx
+++ MovieMunchies-Client/src/components/Cart Component/CartLayout.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import NavbarLayout from "../Navbar Component/NavbarLayout";
+import "./Cart.css";
 
 const CartLayout = ({ cartItems, removeItem }) => {
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+
   return (
     <div>
       <div className="container-fluid m-2">
@@ -9,16 +12,12 @@ const CartLayout = ({ cartItems, removeItem }) => {
       </div>
       <div className="container-fluid mt-5">
         <div className="row">
+          <h4 className="h5 text-light mt-3">Your Cart:</h4>
           {cartItems.map((item, index) => (
             <div className="col-sm-3 mt-3" key={index}>
               <div className="card text-light text-center">
                 <div className="card-body">
-                  <img
-                    src={item.image}
-                    className="card-img-top itemimage"
-                    alt={item.name}
-                  />
-                  <h5 className="card-title m-2">{item.name}</h5>
+                  <h5 className="card-title m-2">{item.name.toUpperCase()}</h5>
                   <p className="card-text mt-3">
                     Cost: {item.price} (tax included)
                   </p>{" "}
@@ -37,6 +36,20 @@ const CartLayout = ({ cartItems, removeItem }) => {
           ))}
         </div>
       </div>
+
+      <footer className="footer">
+        <div className="container-fluid d-flex justify-content-end">
+          <div className="col-sm-3 mt-3 total-card">
+            <div className="card text-light text-center">
+              <div className="card-body">
+                <h5 className="card-title m-2">Total</h5>
+                <p className="card-text mt-3">Total Price: {totalPrice} </p>
+                <button className="btn btn-success">Place Order</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
