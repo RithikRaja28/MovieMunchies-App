6a021e418b9c086b770f2b82e3af18ecb123e8f0
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-03-27 21:26:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://movie-munchies.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
